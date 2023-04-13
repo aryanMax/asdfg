34f142250b349888e2087bcc7390c785d42aecf9
Aryan Rana <105802182+aryanMax@users.noreply.github.com>
2023-04-13 10:50:22 +0530

    Update config.py

--- p_scrapper/Bot/config.py
+++ p_scrapper/Bot/config.py
@@ -3,6 +3,6 @@ from time import time
 
 
 botStartTime = time()
-BOT_TOKEN = getenv("BOT_TOKEN")
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv(5706304938:AAFgJIphAyOXsLh3YqJRh-KrrlQg6O8agbI)
+API_ID = int(getenv(12155241))
+API_HASH = getenv(5d4fb21990c47b88df74dc1611a07483)
